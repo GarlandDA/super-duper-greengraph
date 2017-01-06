6e6f5bc6bda3c700f87f0b6262e598792ae2d095
Diego Aparicio <diego.aparicio.16@ucl.ac.uk>
2017-01-06 15:39:06 +0000

    Full set of tests for graph.py

--- greengraph/test/test_graph.py
+++ greengraph/test/test_graph.py
@@ -3,10 +3,11 @@ import geopy
 import numpy
 import yaml
 import os
-from unittest.mock import Mock
+from unittest.mock import MagicMock
 from unittest.mock import patch
 import unittest.mock as mock
 from greengraph.graph import Greengraph
+from greengraph.map import Map
 from nose.tools import assert_equal, assert_almost_equal
 
 os.chdir('C:\\Users\\Diego\\Documents\\greengraph\\greengraph\\test')
@@ -20,8 +21,8 @@ def test_Greengraph():
 def test_geolocate():
     g = Greengraph('','')
     with mock.patch.object(g.geocoder, 'geocode') as mock_geocode:
-        london_location = g.geolocate('London')
-        mock_geocode.assert_called_with('London', exactly_one=False)
+        london_location = g.geolocate('some place on Earth')
+        mock_geocode.assert_called_with('some place on Earth', exactly_one=False)
 
 def test_location_sequence():
     with open(os.path.join(os.path.dirname(__file__),
@@ -31,3 +32,17 @@ def test_location_sequence():
             answer = fixture.pop('answer')
             g = Greengraph('','')
             numpy.testing.assert_array_equal(g.location_sequence(**fixture), answer)
+            
+def test_green_between():
+    with open(os.path.join(os.path.dirname(__file__),
+                           'fixtures','location_green.yaml')) as fixtures_file:
+        fixtures = yaml.load(fixtures_file)
+        
+        answer = []
+        greenness = []
+        
+        for fixture in fixtures:
+            answer.append(fixture.pop('green'))
+            greenness.append(Map(fixture.pop('lat'),fixture.pop('long')).count_green())
+
+        numpy.testing.assert_array_equal(answer,greenness)
